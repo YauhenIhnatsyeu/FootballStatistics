f65df4137d78c1af9072fbea98ffc274d524cd30
Yauhen Ihnatsyeu <yauhen.ihnatsyeu@itechart-group.com>
2018-03-06 09:32:38 +0300

    Added webpack, react, babel.

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require("path");
+
+module.exports = {
+  entry: "./src/js/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
