942b0385949dabce104144a36ac155dfda3ad343
Yauhen Ihnatsyeu <yauhen.ihnatsyeu@itechart-group.com>
2018-03-07 08:57:42 +0300

    Added tabs' functionality.

--- src/js/components/tabpanel.js
+++ src/js/components/tabpanel.js
@@ -14,22 +14,22 @@ export default class TabPanel extends React.Component {
                 </Tab>
                 <Tab title="German 1. Bundesliga" isCurrent={false}>
                     <span>
-                        Sagittis orci a scelerisque purus. Auctor eu augue ut lectus arcu bibendum at varius. Porta lorem mollis aliquam ut porttitor. Nullam ac tortor vitae purus faucibus. Nisl suscipit adipiscing bibendum est ultricies integer quis auctor elit. Ipsum dolor sit amet consectetur adipiscing elit duis tristique sollicitudin. Pulvinar elementum integer enim neque volutpat ac tincidunt. Diam donec adipiscing tristique risus nec.
+                        Lectus magna fringilla urna porttitor rhoncus dolor. Morbi tristique senectus et netus et malesuada fames ac turpis. Feugiat in fermentum posuere urna nec. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Leo a diam sollicitudin tempor id eu nisl. Mattis ullamcorper velit sed ullamcorper morbi. Mauris vitae ultricies leo integer malesuada nunc. Eget mi proin sed libero enim sed faucibus.
                     </span>
                 </Tab>
                 <Tab title="Spanish Primera" isCurrent={false}>
                     <span>
-                        Sagittis orci a scelerisque purus. Auctor eu augue ut lectus arcu bibendum at varius. Porta lorem mollis aliquam ut porttitor. Nullam ac tortor vitae purus faucibus. Nisl suscipit adipiscing bibendum est ultricies integer quis auctor elit. Ipsum dolor sit amet consectetur adipiscing elit duis tristique sollicitudin. Pulvinar elementum integer enim neque volutpat ac tincidunt. Diam donec adipiscing tristique risus nec.
+                        Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar. Scelerisque eleifend donec pretium vulputate sapien nec sagittis. Ac turpis egestas sed tempus. Tortor vitae purus faucibus ornare. Volutpat diam ut venenatis tellus in metus. Tincidunt augue interdum velit euismod in. Lectus mauris ultrices eros in cursus. Porta non pulvinar neque laoreet suspendisse interdum. Aenean sed adipiscing diam donec adipiscing tristique risus.
                     </span>
                 </Tab>
                 <Tab title="Italian Serie A" isCurrent={false}>
                     <span>
-                        Sagittis orci a scelerisque purus. Auctor eu augue ut lectus arcu bibendum at varius. Porta lorem mollis aliquam ut porttitor. Nullam ac tortor vitae purus faucibus. Nisl suscipit adipiscing bibendum est ultricies integer quis auctor elit. Ipsum dolor sit amet consectetur adipiscing elit duis tristique sollicitudin. Pulvinar elementum integer enim neque volutpat ac tincidunt. Diam donec adipiscing tristique risus nec.
+                        Aenean sed adipiscing diam donec adipiscing tristique risus nec feugiat. Enim lobortis scelerisque fermentum dui faucibus. Nisl rhoncus mattis rhoncus urna neque. Id aliquet lectus proin nibh nisl. Eget mi proin sed libero enim sed faucibus turpis in. Gravida cum sociis natoque penatibus et magnis dis. Elit ut aliquam purus sit amet luctus venenatis lectus. Lacus vel facilisis volutpat est.
                     </span>
                 </Tab>
                 <Tab title="French League 1" isCurrent={false}>
                     <span>
-                        Sagittis orci a scelerisque purus. Auctor eu augue ut lectus arcu bibendum at varius. Porta lorem mollis aliquam ut porttitor. Nullam ac tortor vitae purus faucibus. Nisl suscipit adipiscing bibendum est ultricies integer quis auctor elit. Ipsum dolor sit amet consectetur adipiscing elit duis tristique sollicitudin. Pulvinar elementum integer enim neque volutpat ac tincidunt. Diam donec adipiscing tristique risus nec.
+                        Integer malesuada nunc vel risus commodo. Pretium aenean pharetra magna ac. Quis auctor elit sed vulputate mi. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Eget arcu dictum varius duis at consectetur. Ac feugiat sed lectus vestibulum mattis ullamcorper. Magna fringilla urna porttitor rhoncus dolor. Ac tortor vitae purus faucibus ornare suspendisse sed. Leo integer malesuada nunc vel risus commodo viverra maecenas accumsan. Sagittis vitae et leo duis.
                     </span>
                 </Tab>
             </Tabs>
--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -1,27 +1,63 @@
 import React from "react";
 
+import Tab from "./tab";
+
 export default class Tabs extends React.Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            currentTab: 0
+        }
+
+        this.tabElements = [];
+    }
+    
+    handleClick = (tabIndex) => {
+        this.setState({
+            currentTab: tabIndex
+        });
+    }
+
+    updateTabsElements = () => {
+        this.tabElements = [];
+        this.props.children.map((child) => {
+            if (child.type === Tab) {
+                this.tabElements.push(child);
+            }
+        })
+    }
+
     getTabsList = () => {
         return (
             <ul>
-                {this.props.children.map((child, index) =>
-                    <li key={index}>{child.props.title}</li>
+                {this.tabElements.map((tab, tabIndex) =>
+                    <li key={tabIndex} onClick={() => this.handleClick(tabIndex)}>{tab.props.title}</li>
                 )}
             </ul>
         );
     }
 
     getCurrentTabContent = () => {
-        //console.log(this.props.children);
-        for (let i = 0; i < this.props.children.length; i++) {
-            if (this.props.children[i].props.isCurrent === true) {
-                return this.props.children[i].props.children;
-            }
-        }
-        return "";
+        // for (let i = 0; i < this.props.children.length; i++) {
+        //     if (this.props.children[i].props.isCurrent === true) {
+        //         return this.props.children[i].props.children;
+        //     }
+        // }
+        // this.tabElements.map((tab, tabIndex) => {
+        //     if (tab.props.isCurrent === true) {
+                
+        //     }
+        // })
+        // return "";
+        console.log(this.tabElements[0]);
+        console.log(this.state.currentTab);
+        
+        return this.tabElements[this.state.currentTab].props.children;
     }
 
     render() {
+        this.updateTabsElements();
         return (
             <div className="tabs">
                 {this.getTabsList()}
