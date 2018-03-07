f73d8de88f157e5a63048d4b5a1f71ca897cf5fe
Yauhen Ihnatsyeu <yauhen.ihnatsyeu@itechart-group.com>
2018-03-07 11:04:37 +0300

    Added styles and data to table.

--- src/js/components/table.js
+++ src/js/components/table.js
@@ -4,28 +4,30 @@ export default class Table extends React.Component {
     render() {
         return (
             <table className="table">
-                {this.props.content.slice(0, 1).map((row) => {
-                    return (
-                    <tr>
-                        {row.map((col) => {
-                            return (
-                            <th>
-                                {col}
-                            </th>
-                        )})}
-                    </tr>
-                )})}
-                {this.props.content.slice(1).map((row) => {
-                    return (
-                    <tr>
-                        {row.map((col) => {
-                            return (
-                            <td>
-                                {col}
-                            </td>
-                        )})}
-                    </tr>
-                )})}
+                <tbody>
+                    {this.props.content.slice(0, 1).map((row, index) => {
+                        return (
+                        <tr className="table__row table__row_header" key={index}>
+                            {row.map((col, index) => {
+                                return (
+                                <th className="table__col table__col_header" key={index}>
+                                    {col}
+                                </th>
+                            )})}
+                        </tr>
+                    )})}
+                    {this.props.content.slice(1).map((row, index) => {
+                        return (
+                        <tr className="table__row" key={index}>
+                            {row.map((col, index) => {
+                                return (
+                                <td className="table__col" key={index}>
+                                    {col}
+                                </td>
+                            )})}
+                        </tr>
+                    )})}
+                </tbody>
             </table>
         );
     }
