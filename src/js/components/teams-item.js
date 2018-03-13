e234374367d37cddd8be9c263486c7f0ef18f58f
Yauhen Ihnatsyeu <yauhen.ihnatsyeu@itechart-group.com>
2018-03-13 12:25:47 +0300

    Added functionalty to add/remove from favourites buttons.

--- src/js/components/teams-item.js
+++ src/js/components/teams-item.js
@@ -3,6 +3,18 @@ import React from "react";
 import {Link} from 'react-router-dom';
 
 export default class TeamsItem extends React.Component {
+    constructor(props) {
+        super(props);
+
+        //Extracting id from team url and store it in the variable
+        this.teamId = this.extractId(this.props.team._links.self.href);
+
+        this.state = {
+            //When teams-item initializes, it checks if it is favourite using local storage
+            isFavourite: this.isThisTeamFavourite()
+        }
+    }
+
     //Extracts id from the ending of the team url
     extractId(url) {
         const regEx = /\d+$/;
@@ -10,8 +22,70 @@ export default class TeamsItem extends React.Component {
         return ids.length === 1 ? ids[0] : 0;
     }
 
+    //Checks if this team is favourite using local storage
+    isThisTeamFavourite() {
+        let favourites = localStorage.getItem("favourites");
+        if (favourites) {
+            favourites = JSON.parse(favourites);
+            const indexOfTeamId = favourites.indexOf(this.teamId);
+            if (indexOfTeamId === -1) {
+                //If local storage doesn't have this team's id, team is not favourite
+                return false;
+            }
+            //If local storage has this team's id, team is favourite
+            return true;
+        }
+        //If local storage doesn't have "favourites" key, team is not favourite
+        return false;
+    }
+
+    //Handles add/remove from favourites button click
+    handleClick = () => {
+        if (this.state.isFavourite) {
+            this.removeTeamIdFromLocalStorage();
+        } else {
+            this.addTeamIdToLocalStorage();
+        }
+        //Change isFavoutite state to opposite
+        this.setState({
+            isFavourite: !this.state.isFavourite
+        });
+    }
+
+    addTeamIdToLocalStorage = () => {
+        let favourites = localStorage.getItem("favourites");
+        if (favourites) {
+            //If local storage has "favourites" key, get its item,
+            favourites = JSON.parse(favourites);
+            //and add new id
+            favourites.push(this.teamId);
+            favourites = JSON.stringify(favourites);
+        } else {
+            //If local storage doesn't have "favourites" key,
+            //just create an array with one id
+            favourites = JSON.stringify([this.teamId]);
+        }
+        //In the end set favourites item to a new one
+        localStorage.setItem("favourites", favourites);
+    }
+
+    removeTeamIdFromLocalStorage = () => {
+        let favourites = localStorage.getItem("favourites");
+        if (favourites) {
+            favourites = JSON.parse(favourites);
+            //Getting index of this team's id in array
+            const indexOfTeamId = favourites.indexOf(this.teamId);
+            if (indexOfTeamId !== -1) {
+                //Removing id from array
+                favourites.splice(indexOfTeamId, 1);
+                favourites = JSON.stringify(favourites);
+                localStorage.setItem("favourites", favourites);
+            }
+        }
+    }
+
     render() {
-        const teamUrl = "/team/" + this.extractId(this.props.team._links.self.href);
+        const teamUrl = "/team/" + this.teamId;
         return (
             <div className="teams-list__teams-item-container">
                 
@@ -31,7 +105,9 @@ export default class TeamsItem extends React.Component {
                                 this.props.team.squadMarketValue && 
                                 <p className="teams-item__market-value">Squad market value: {this.props.team.squadMarketValue}</p>
                             }
-                            <button className="teams-item__button">Add team to favourite</button>
+                            <button className="teams-item__button" onClick={this.handleClick}>
+                                {this.state.isFavourite ? "Remove" : "Add"} team to favourites
+                            </button>
                         </div>
                     </div>
             </div>
