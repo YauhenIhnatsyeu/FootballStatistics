ee7e5787ef948a52beea4cd923b3e73d0d82bfc4
Yauhen Ihnatsyeu <yauhen.ihnatsyeu@itechart-group.com>
2018-03-16 17:50:58 +0300

    Added jersey number to player item.

--- src/js/components/player-item.js
+++ src/js/components/player-item.js
@@ -5,7 +5,10 @@ export default class MainTeam extends React.Component {
 		return (
 			<div className="item">
                 <div className="player-item__info-container">
-                    <p className="item__name team-item-for-header__name">{this.props.player.name}</p>
+                    <p className="item__name team-item-for-header__name">
+                        {/* If jerseyNumber is null or undefined, don't display it */}
+                        {this.props.player.jerseyNumber && this.props.player.jerseyNumber + ". "} {this.props.player.name}
+                    </p>
                     <p className="player-item__info">Position: {this.props.player.position}</p>
                     <p className="player-item__info">Date of birth: {this.props.player.dateOfBirth}</p>
                     {
